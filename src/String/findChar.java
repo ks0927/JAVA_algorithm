1203bfc825e97c91f61a60991490cd5c91b912eb
ks0927 <brian_als@naver.com>
2022-06-30 15:55:58 +0900

    1-1 done

--- src/String/findChar.java
+++ src/String/findChar.java
@@ -3,7 +3,26 @@ package String;
 import java.util.Scanner;
 
 public class findChar {
+    public int solution(String str, char c){
+        int answer =  0;
+        str = str.toUpperCase();
+        c = Character.toUpperCase(c);
 
+        for(char x : str.toCharArray()){
+            if(x==c) answer++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        findChar F = new findChar();
+        Scanner kb = new Scanner(System.in);
+
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+        System.out.println(F.solution(str,c));
+    }
 
 }
 
