d9ae42818051f2cbdf20299db8cda857d024dc1c
ks0927 <brian_als@naver.com>
2022-07-05 14:16:30 +0900

    2-8 done

---
+++ src/Array/raking.java
@@ -0,0 +1,36 @@
+package Array;
+
+import java.util.Scanner;
+
+public class raking {
+    public int[] solution(int[] arr){
+        int[] answer = new int[arr.length];
+        int cnt = 1;
+        int tmp;
+        for(int i=0; i< arr.length ; i++){
+            tmp =arr[i];
+            for(int j = 0; j< arr.length; j++){
+                if(tmp < arr[j])
+                    cnt++;
+            }
+            answer[i] = cnt;
+            cnt= 1;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        raking R = new raking();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0 ; i< arr.length; i++)
+            arr[i] = kb.nextInt();
+
+        for(int x : R.solution(arr))
+            System.out.print(x+" ");
+
+    }
+}
