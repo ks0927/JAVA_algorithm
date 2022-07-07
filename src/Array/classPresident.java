b408d62132c113e46ae378340ad9b420382ebc8d
ks0927 <brian_als@naver.com>
2022-07-07 21:04:44 +0900

    2-11 done

--- src/Array/classPresident.java
+++ src/Array/classPresident.java
@@ -1,9 +1,33 @@
 package Array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class classPresident {
 
+    public int solution(int n, int[][] arr){
+        int[] knowStudent = new int[n];
+        int answer=0;
+        for(int i=0; i<n; i++){   //비교학생1
+            for(int j=0; j<n; j++){ //비교학생2
+                for(int k=0;k<5; k++){ //학년
+                    if(i==j)  //나일땐 제외
+                        continue;
+                    if(arr[i][k]==arr[j][k]){
+                        knowStudent[i]++;
+                        break;
+                    }
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            if(knowStudent[i]==Arrays.stream(knowStudent).max().getAsInt()) {
+                answer = i + 1;
+                break;
+            }
+        }
+        return answer;
+    }
 
     public static void main(String[] args){
 
@@ -11,11 +35,12 @@ public class classPresident {
         Scanner kb = new Scanner(System.in);
 
         int n = kb.nextInt();
-        int[][] arr = new int[n][n];
+        int[][] arr = new int[n][5];
         for(int i=0; i<n ; i++){
-            for(int j =0; j<n; j++)
+            for(int j =0; j<5; j++)
                 arr[i][j]= kb.nextInt();
         }
 
+        System.out.println(C.solution(n,arr));
     }
 }
