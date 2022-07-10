5e0da9487d68adb53685802cf164bda839ab716b
ks0927 <brian_als@naver.com>
2022-07-10 12:23:28 +0900

    3-3 done

---
+++ src/TwoPointers/maxSales.java
@@ -0,0 +1,32 @@
+package TwoPointers;
+
+import java.util.Scanner;
+
+public class maxSales {
+    public int solution(int n , int k, int[] arr){
+        int max = 0;
+        int answer =0;
+        for(int i=0; i<k; i++)
+            max+=arr[i];
+        answer=max;
+        for(int i= k; i<n ; i++){
+            max+= (arr[i]-arr[i-k]);
+            if(max>answer)
+                answer = max;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        maxSales M = new maxSales();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] arr = new int[n];
+        for(int i= 0; i< arr.length; i++)
+            arr[i]= kb.nextInt();
+
+        System.out.println(M.solution(n,k,arr));
+    }
+}
