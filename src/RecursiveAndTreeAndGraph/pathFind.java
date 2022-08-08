5e2f31113443d478a04cce7fb36699fc08f06c0c
ks0927 <brian_als@naver.com>
2022-08-08 22:16:59 +0900

    7-12 done

---
+++ src/RecursiveAndTreeAndGraph/pathFind.java
@@ -0,0 +1,38 @@
+package RecursiveAndTreeAndGraph;
+
+import java.util.Scanner;
+
+public class pathFind {
+    static int n, m, answer=0;
+    static int[][] graph;
+    static int[] ch;
+    public void DFS(int v){
+        if(v==n) answer++;
+        else{
+            for(int i=1; i< graph.length; i++){
+                if(graph[v][i]==1 && ch[i]==0){
+                    ch[i]=1;
+                    DFS(i);
+                    ch[i]=0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        pathFind T = new pathFind();
+        Scanner kb = new Scanner(System.in);
+        n=kb.nextInt();
+        m=kb.nextInt();
+        graph=new int[n+1][n+1];
+        ch=new int[n+1];
+        for(int i=0; i<m; i++){
+            int a=kb.nextInt();
+            int b=kb.nextInt();
+            graph[a][b]=1;
+        }
+        ch[1]=1;
+        T.DFS(1);
+        System.out.println(answer);
+    }
+}
