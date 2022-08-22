2393b46c21fbaf60c1141bc9f644cfe7e99afbb7
ks0927 <brian_als@naver.com>
2022-08-22 21:11:36 +0900

    9-1 done

--- src/Greedy/koreanWrestling.java
+++ src/Greedy/koreanWrestling.java
@@ -7,15 +7,15 @@ public class koreanWrestling {
     public int solution(int n,int[][] arr) {
         Arrays.sort(arr, ((o1, o2) -> {
             if (o1[0] == o2[0]) {
-                return Integer.compare(o2[1], o1[1]);
+                return Integer.compare(o2[1], o1[1]); //양수일땐 내림차순, 음수일땐 오름차순? o1-o2를 한다고 생각하자?o2가 o1보다 크다고 생각하고?
             } else {
                 return Integer.compare(o2[0], o1[0]);
             }
         }));
 
-        for (int i = 0; i < n; i++) {
+/*        for (int i = 0; i < n; i++) {
             System.out.println(arr[i][0] + " " + arr[i][1]);
-        }
+        }*/
         int max =Integer.MIN_VALUE;
         int answer=0;
         for (int i = 0; i < n; i++) {
