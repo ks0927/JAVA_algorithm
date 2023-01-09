897ca4633f3b541d709055c78e5461c7aa51a8df
ks0927 <brian_als@naver.com>
2023-01-09 15:03:25 +0900

    1-3 additional solution

--- src/String/LongestWord.java
+++ src/String/LongestWord.java
@@ -3,6 +3,7 @@ package String;
 import java.util.Scanner;
 
 public class LongestWord {
+    //split을 사용해서 길이 비교
     public String solution(String str){
         String answer ="";
         int m = Integer.MIN_VALUE;
@@ -15,12 +16,30 @@ public class LongestWord {
         }
         return answer;
     }
+    //substring을 통한 split방법
+    public String solution2(String str){
+        String answer = "";
+        int m = Integer.MIN_VALUE,pos;
+        while((pos = str.indexOf(' '))!= -1){
+            String temp = str.substring(0,pos); //단어 뽑아내기
+            int len = temp.length();
+            if(len > m){
+                m= len;
+                answer = temp;
+            }
+            str = str.substring(pos+1); //뽑아낸 단어 이후의 str로 재변경
+        }
+        if(str.length()>m)
+            answer=str;
+        return  answer;
 
+    }
     public static void main(String[] args){
         LongestWord L = new LongestWord();
         Scanner kb = new Scanner(System.in);
 
         String str = kb.nextLine();
         System.out.println(L.solution(str));
+        System.out.println(L.solution2(str));
     }
 }
