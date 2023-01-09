7aeed98e80aacfc5f2219b81143c1a34b024fe30
ks0927 <brian_als@naver.com>
2023-01-09 13:56:48 +0900

    1-2 additional solution

--- src/String/transUpperLower.java
+++ src/String/transUpperLower.java
@@ -3,6 +3,7 @@ package String;
 import java.util.Scanner;
 
 public class transUpperLower {
+    //Character 함수를 이용하는 경우
     public String solution(String str){
         String answer ="";
 
@@ -14,6 +15,20 @@ public class transUpperLower {
         }
         return answer;
     }
+    //아스키 코드를 이용하는 경우
+    public String solution2(String str){
+        String answer ="";
+
+        for (char c : str.toCharArray()) {
+            if(65<=c && c<=90){ //대문자면
+                answer+=(char)(c+32); //32를 더해서 소문자로 만들어준다.
+            }
+            else{//소문자면
+                answer+=(char)(c-32); //32를 빼서 대문자로 만들어준다.
+            }
+        }
+        return answer;
+    }
 
     public static void main(String[] args){
 
@@ -21,5 +36,6 @@ public class transUpperLower {
         Scanner kb = new Scanner(System.in);
         String str = kb.next();
         System.out.println(T.solution(str));
+        System.out.println(T.solution2(str));
     }
 }
