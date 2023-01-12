36100ae0e3b9c2663b8656f0c578ad9f37409fb3
ks0927 <brian_als@naver.com>
2023-01-12 21:45:32 +0900

    add comment

--- src/Array/eratosthenes.java
+++ src/Array/eratosthenes.java
@@ -7,9 +7,11 @@ public class eratosthenes {
         int[] arr = new int[n+1];
         int answer =0;
         for(int i = 2 ; i<= n ; i++){
+            //처음보는거면(0이면) 소수갯수 추가
             if(arr[i]==0){
                 answer++;
             }
+            //본것과 그 배수들을 모두 1로 해서 지운다.
             for(int j = i ; j<=n ; j+=i)
                 arr[j]=1;
         }
