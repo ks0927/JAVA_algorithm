369bf4beab5a207bf0d83e9ba74d901b538fab6a
ks0927 <brian_als@naver.com>
2023-01-12 20:41:53 +0900

    2-4 additional solution

--- src/Array/fibonacci.java
+++ src/Array/fibonacci.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class fibonacci {
+    //배열을 사용하는 피보나치
     private static int[] solution(int n) {
         int[] answer= new int[n];
         answer[0]=1;
@@ -14,6 +15,19 @@ public class fibonacci {
 
         return answer;
     }
+    //배열없이 변수를 통해 사용하는 피보나치
+    private void solution2(int n){
+        int a = 1;
+        int b = 1;
+        int c;
+        System.out.print(a +" " +b +" ");
+        for (int i = 2; i < n; i++) {
+            c =a + b;
+            System.out.print(c + " ");
+            a =b;
+            b =c;
+        }
+    }
 
     public static void main(String[] args){
         fibonacci F = new fibonacci();
@@ -23,6 +37,8 @@ public class fibonacci {
         for(int x : arr){
             System.out.print(x+" ");
         }
+        System.out.println();
+        F.solution2(n);
     }
 
 }
