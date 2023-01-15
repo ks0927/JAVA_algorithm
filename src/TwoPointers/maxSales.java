279cc0e8a057529c388547ec6253688d604a1460
ks0927 <brian_als@naver.com>
2023-01-15 20:37:03 +0900

    3-3 add comment

--- src/TwoPointers/maxSales.java
+++ src/TwoPointers/maxSales.java
@@ -6,9 +6,12 @@ public class maxSales {
     public int solution(int n , int k, int[] arr){
         int max = 0;
         int answer =0;
+        //초기 sliding window 값 설정
         for(int i=0; i<k; i++)
             max+=arr[i];
         answer=max;
+
+        //한칸씩 밀면서 max값 초기화
         for(int i= k; i<n ; i++){
             max+= (arr[i]-arr[i-k]);
             if(max>answer)
