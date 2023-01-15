7b12cc181a42c8804ea3c0315f254013d06cc55c
ks0927 <brian_als@naver.com>
2023-01-15 21:22:06 +0900

    3-4 additional solution

--- src/TwoPointers/continuousPartialSequence.java
+++ src/TwoPointers/continuousPartialSequence.java
@@ -3,6 +3,7 @@ package TwoPointers;
 import java.util.Scanner;
 
 public class continuousPartialSequence {
+    //뭔가 엉성한 two pointer
     public int solution(int n, int m, int[] arr){
         int cnt1=0, cnt2=0;
         int tmp=0;
@@ -25,7 +26,19 @@ public class continuousPartialSequence {
         }
 
         return answer;
-
+    }
+    // two pointer와 sliding window의 결합
+    public int solution2(int n, int m, int[] arr){
+        int answer=0, sum=0, lt=0, rt;
+        for(rt=0; rt<n; rt++){
+            sum+=arr[rt]; //하나씩 추가
+            if(sum==m) answer++; //조건에 부합할때 +1
+            while(sum>=m){ //초과되었을때
+                sum-=arr[lt++]; //앞에서 부터 줄임 sliding window
+                if(sum==m) answer++; //줄였을때 조건에 부합할수도있으니까
+            }
+        }
+        return answer;
     }
 
     public static void main(String[] args){
@@ -39,5 +52,6 @@ public class continuousPartialSequence {
             arr[i]= kb.nextInt();
 
         System.out.println(C.solution(n,m,arr));
+        System.out.println(C.solution2(n,m,arr));
     }
 }
