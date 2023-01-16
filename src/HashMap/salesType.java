7154eed29924625376f7b6a92a36e19534933f80
ks0927 <brian_als@naver.com>
2023-01-16 22:33:38 +0900

    4-3 add comment

--- src/HashMap/salesType.java
+++ src/HashMap/salesType.java
@@ -13,8 +13,10 @@ public class salesType {
             answer[i]=set.size();
             set.clear();
         }*/
+        //map과 sliding window의 결합
         Map<Integer,Integer> slide = new HashMap<>();
         ArrayList<Integer> answer= new ArrayList<>();
+
         for(int i=0; i<k; i++)
             slide.put(arr[i],slide.getOrDefault(arr[i],0)+1);
         answer.add(slide.size());
@@ -22,8 +24,8 @@ public class salesType {
         for(int j= k; j<n;j++){
             slide.put(arr[j],slide.getOrDefault(arr[j],0)+1);
             slide.put(arr[j-k],slide.getOrDefault(arr[j-k],0)-1);
-            if(slide.get(arr[j-k])==0)
-                slide.remove(arr[j-k]);
+            if(slide.get(arr[j-k])==0) //value가 0이 되어도 키는 남아있기때문에 지워줘야한다.
+                slide.remove(arr[j-k]); //remove로 key-value 삭제
             answer.add(slide.size());
         }
         return answer;
