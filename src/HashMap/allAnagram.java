bfd893cbd69c64d59e94cb05306a0a2273959b6c
ks0927 <brian_als@naver.com>
2023-01-17 20:24:32 +0900

    4-4 additional solution

--- src/HashMap/allAnagram.java
+++ src/HashMap/allAnagram.java
@@ -44,6 +44,31 @@ public class allAnagram {
         }
         return answer;
     }
+    //equals 함수를 사용한 버전
+    public int solution2(String str, String word){
+        int answer= 0;
+        Map<Character,Integer> map = new HashMap<>();
+        Map<Character,Integer> target = new HashMap<>();
+
+        for(char x : word.toCharArray())
+            target.put(x,target.getOrDefault(x,0)+1); // 타겟문자열 map에 추가
+
+        int k = word.length()-1;
+        for (int i = 0; i < k; i++) {
+            map.put(str.charAt(i),map.getOrDefault(str.charAt(i),0)+1);
+        }
+        //sliding window
+        for (int i = k; i < str.length(); i++) {
+            map.put(str.charAt(i),map.getOrDefault(str.charAt(i),0)+1);  //하나밀고
+            if(map.equals(target))  //equals 함수를 사용하면 entryset 즉 모든 키값과 value를 꺼내서 비교하는듯
+                answer++;
+            map.put(str.charAt(i-k),map.get(str.charAt(i-k))-1); //밀었으니까 앞에꺼 지우고
+            if(map.get(str.charAt(i-k))==0)  //value값이 0이면 key-value 쌍 삭제제
+               map.remove(str.charAt(i-k));
+        }
+
+        return answer;
+    }
 
 
     public static void main(String[] args){
@@ -53,5 +78,6 @@ public class allAnagram {
         String  str = kb.next();
         String  word = kb.next();
         System.out.println(A.solution(str,word));
+        System.out.println(A.solution2(str,word));
     }
 }
