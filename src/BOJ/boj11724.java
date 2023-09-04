be8efb3fede3fc03f7d9059adbaba2092bd3f84a
ks0927 <brian_als@naver.com>
2023-09-04 15:24:07 +0900

    boj 11724

---
+++ src/BOJ/boj11724.java
@@ -0,0 +1,72 @@
+package BOJ;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class boj11724 {
+
+
+    //인접 리스트로 무방향 그래프 설정
+    static ArrayList<ArrayList<Integer>> graph;
+    //해당 정점이 연결되었나 체크(내부 숫자를 통해 누구누구하고 같이 연결되어있는지도 확인가능
+    static int[] visited;
+    static int N;
+    static int M;
+    //연결 요소 (Connected Component)의 개수
+    static int CC;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new int[N+1];
+
+        //그래프 초기화
+        graph = new ArrayList<>();
+        for (int i = 0; i < N + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        CC =0;
+        for (int i = 1; i <= N; i++) {
+            //아직 연결된적이 없다면
+            if(visited[i]==0){
+                ++CC;
+                solution(i,CC); //dfs를 불러서 같이 연결된 모든 정점을 찾아서 같은 visited 값을 가지게한다.
+            }
+        }
+        System.out.println(CC);
+
+        for (int i : visited) {
+            System.out.print(i+" ");
+        }
+
+    }
+
+    static void solution(int x, int cc){
+
+        visited[x] = cc;
+
+        ArrayList<Integer> integers = graph.get(x);
+        for (Integer integer : integers) {
+            if(visited[integer]==0){
+                visited[integer]=cc;
+                solution(integer,cc);
+            }
+        }
+
+    }
+}
