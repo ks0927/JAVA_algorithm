9ed551ef8ca5e16d02c72c02792423cfb82cf252
ks0927 <brian_als@naver.com>
2023-09-28 01:23:26 +0900

    boj 2141

--- src/BOJ/boj2141.java
+++ src/BOJ/boj2141.java
@@ -3,11 +3,37 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class boj2141 {
 
+    static class Country implements Comparable<Country>{
+        int i;
+        int people;
+
+        public Country(int i, int people) {
+            this.i = i;
+            this.people = people;
+        }
+
+        @Override
+        public int compareTo(Country o) {
+            return this.i - o.i;
+        }
+
+/*        @Override
+        public String toString() {
+            return "Country{" +
+                    "i=" + i +
+                    ", people=" + people +
+                    '}';
+        }*/
+    }
     static int N;
+    static List<Country> country;
 
     public static void main(String[] args) throws IOException {
 
@@ -16,17 +42,38 @@ public class boj2141 {
 
         N = Integer.parseInt(st.nextToken());
 
-//        포문으로 우체국이 될수있는 위치를 모두 돌면서 해당위치에 우체국이 놓아졌을때
-//        좌우의 사람들을 다시 모두 돌면서 거리의 합을 구하면 모든 경우의 수가 나오고
-//        해당 값들중 가장작을때 이면서 가장 인덱스가 작은곳을 구하려했다.
-//
-//        허나 마을의 갯수는 1 ≤ N ≤ 100,000 이고 해당 루프를 돌면서 내부에서 좌우루프를 N만큼 다시 돌텐데
-//        그러면 최악의 경우 10,000,000,000의 시행이 이어질것이라 생각했고 해당문제는 특정 요건이 있을거라 판단했다.
-//                딱 생각나는 것은 없어서 이전의 그리디 문제들을 돌아보다가 이전의 백준 20300의 서강근육맨이 생각났다.
-//                해당 문제와 같은 풀이는 아니지만 해당문제에서 팀을 이룰때 값을 고르게 고르는 상황을 유도했었다.
-//        그래서 이번 문제도 그러면 우체국의 위치로부터 좌 우의 마을사람들이 비슷하게 혹은 같게 되는 상황일때 최소일수있나? 가정했다.
-//
-//                문제는 구현을 어떻게 하면 좌우를 비교하는데 시간을 적게 진행할까... 즉 그리디 하게 하는 방식을 고민했다.
-//        그냥 앞에서부터 더하다가 인구수의 절반이 나온 시점에 우체국을 놓으면 그게 좌우가 비슷한 인구수로 배치되지않을까 생각.
+        country = new ArrayList<>();
+
+        Long allPeople = 0L;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int index = Integer.parseInt(st.nextToken());
+            int people = Integer.parseInt(st.nextToken());
+
+            allPeople += people;
+
+            country.add(new Country(index,people));
+        }
+
+        Collections.sort(country);
+
+/*        for (Country country1 : country) {
+            System.out.println("country1 = " + country1);
+        }*/
+
+        Long halfAllPeople = (allPeople + 1) / 2;
+
+        // 우체국이 좌우에 같거나 비슷한 숫자가 형성되는 고른 상태일때 최소값이 나올것이라 예상
+        // 그렇기에 모든 사람의 수의 절반이 넘거나 같은 상황이 된순간의 마을의 번지를 반환한다.
+       Long countPeople = 0L;
+        for (int i = 0; i < N; i++) {
+
+            int nowPeople = country.get(i).people;
+            countPeople += nowPeople;
+            if(countPeople >= halfAllPeople){
+                System.out.println(country.get(i).i);
+                break;
+            }
+        }
     }
 }
