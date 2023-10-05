5aa27288833c3a58acc58a915b1aaf746e42a562
ks0927 <brian_als@naver.com>
2023-10-05 15:43:28 +0900

    boj 1463 dp

--- src/BOJ/boj1463.java
+++ src/BOJ/boj1463.java
@@ -3,11 +3,14 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class boj1463 {
+    static int[] memo;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -19,9 +22,61 @@ public class boj1463 {
             System.out.println(0);
             return;
         }
+
+        if(X == 2 || X == 3){
+            System.out.println(1);
+            return;
+        }
+
+        memo = new int[X+1];
+
+        Arrays.fill(memo,Integer.MAX_VALUE);
+
+        memo[1] = 0;
+        memo[2] = 1;
+        memo[3] = 1;
+
+
+
+        for (int i = 1; i <= X; i++) {
+            if(i % 3 == 0){
+                int value = 0;
+                int n3 = value + 1;
+                int nextValue = i / 3;
+                value = memo[nextValue] + n3;
+                memo[i] = Math.min(memo[i],value);
+            }
+            if(i % 2 == 0){
+                int value = 0;
+                int n2 = value + 1;
+                int nextValue = i / 2;
+                value = memo[nextValue] + n2;
+                memo[i] = Math.min(memo[i],value);
+            }
+            if(i != 1){
+                int value = 0;
+                int nMinus = value + 1;
+                int nextValue = i-1;
+                value = memo[nextValue] + nMinus;
+                memo[i] = Math.min(memo[i],value);
+            }
+
+        }
+
+        System.out.println(memo[X]);
+
+
+
+
+/*
+        if(X == 1){
+            System.out.println(0);
+            return;
+        }
         int bfs = BFS(X);
 
         System.out.println(bfs);
+*/
 
     }
 
