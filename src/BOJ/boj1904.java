d717d173ae9ae8f630f89f48eb0497f35a1d6ba4
ks0927 <brian_als@naver.com>
2023-10-05 18:17:13 +0900

    boj 1904

---
+++ src/BOJ/boj1904.java
@@ -0,0 +1,38 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj1904 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        if(N == 1){
+            System.out.println(1);
+            return;
+        }
+        if(N == 2){
+            System.out.println(2);
+            return;
+        }
+
+        Long[] memo = new Long[N+1];
+
+        memo[1] = 1L;
+        memo[2] = 2L;
+
+        for (int i = 3; i <= N; i++) {
+            Long curValue = (memo[i - 1] + memo[i - 2]) % 15746;
+            memo[i] = curValue;
+        }
+
+        System.out.println(memo[N]);
+    }
+}
