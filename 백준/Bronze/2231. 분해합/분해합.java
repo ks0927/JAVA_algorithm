67ee9e727e0f0fa3b0919d887cca7fb70a962de8
사한 <brian_als@naver.com>
2023-11-27 14:21:29 +0900

    [Bronze II] Title: 분해합, Time: 160 ms, Memory: 18288 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        arr = new int[1_000_001];
+
+        for (int i = 1; i < 1_000_001; i++) {
+            arr[i]=(splitInt(i));
+        }
+
+        for (int i = 0; i < 1_000_001; i++) {
+            if(arr[i] == N){
+                System.out.println(i);
+                return;
+            }
+        }
+        System.out.println(0);
+    }
+
+
+    public static int splitInt(int num){
+        int answer = num;
+
+        while(true){
+
+            if(num / 10 != 0){
+                answer += num %10;
+                num/= 10;
+            }else {
+                answer += num;
+                break;
+            }
+        }
+        return answer;
+    }
+}
