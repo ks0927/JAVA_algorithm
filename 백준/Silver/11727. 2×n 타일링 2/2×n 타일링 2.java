9b5d4d432820d482f3ba80f761e65b3fb717d9b0
사한 <brian_als@naver.com>
2024-01-27 17:25:21 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 132 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        if(N == 1){
+            System.out.println(1);
+            return;
+        }
+        if(N == 2){
+            System.out.println(3);
+            return;
+        }
+
+        Long[] memo = new Long[N+1];
+
+        memo[1] = 1L;
+        memo[2] = 3L;
+
+        for (int i = 3; i <= N; i++) {
+            Long curValue = ( memo[i - 1] + (memo[i - 2]*2) ) % 10007;
+            memo[i] = curValue;
+        }
+
+        System.out.println(memo[N]);
+    }
+}
