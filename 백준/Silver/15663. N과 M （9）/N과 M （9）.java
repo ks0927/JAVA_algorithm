e876a51a6f1d2e5d72b787b89d4de4ec1da276fc
사한 <brian_als@naver.com>
2024-04-10 18:58:41 +0900

    [Silver II] Title: N과 M (9), Time: 196 ms, Memory: 20736 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] lines;
+    static int[] map;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        map = new int[N];
+        visited = new boolean[N];
+        lines = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            map[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(map);
+
+        DFS(N, M, 0);
+
+        System.out.println(sb);
+    }
+
+    static void DFS(int N, int M, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(lines[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        int before = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+
+                if (before != map[i]) {
+                    visited[i] = true;
+                    lines[depth] = map[i];
+                    before = map[i];
+                    DFS(N, M, depth + 1);
+                    visited[i] = false;
+                }
+            }
+
+        }
+    }
+
+
+}
