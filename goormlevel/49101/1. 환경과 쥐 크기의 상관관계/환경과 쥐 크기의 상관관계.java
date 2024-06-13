1fe9b4e44e5795f85370c5b718cee1b7a2cc013a
사한 <brian_als@naver.com>
2024-06-13 21:25:57 +0900

    [goormlevel] Title: 환경과 쥐 크기의 상관관계, Time: 0.14 ms, Memory: 34.39 MB, Difficulty: 2 -BaekjoonHub

--- goormlevel/49101/1. 환경과 쥐 크기의 상관관계/환경과 쥐 크기의 상관관계.java
+++ goormlevel/49101/1. 환경과 쥐 크기의 상관관계/환경과 쥐 크기의 상관관계.java
@@ -5,10 +5,10 @@ class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		
+
 		int[] aSpot = new int[100_001];
 		int[] bSpot = new int[100_001];
-		
+
 		int aBest = 0;
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < T; i++) {
@@ -18,7 +18,7 @@ class Main {
 				aBest = mice;
 			}
 		}
-		
+
 		int bBest = 0;
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < T; i++) {
@@ -28,10 +28,10 @@ class Main {
 				bBest = mice;
 			}
 		}
-		
+
 		int aAnswer = getBestSpotValue(aSpot,aBest);
 		int bAnswer = getBestSpotValue(bSpot,bBest);
-		
+
 		StringBuilder sb = new StringBuilder();
 		sb.append(aAnswer).append(" ").append(bAnswer).append("\n");
 		if(aAnswer > bAnswer){
@@ -42,28 +42,28 @@ class Main {
 
 		System.out.println(sb);
 	}
-	
+
 	//각 지점의 대표값을 찾는 메서드
 	static int getBestSpotValue(int[] spot, int maxValue) {
 		int Answer = 0;
 		int Sum = 0;
-		
+
 		for(int i = 1; i <= maxValue; i++) {
 			int fore = i - 2;
 			if(fore < 1){
 				fore = 1;
 			}
-			
+
 			int back = i + 2;
 			if(back > 100_000){
 				back = 100_000;
 			}
-			
+
 			int sum = 0;
 			for(int j = fore; j <= back; j++) {
 				sum += spot[j];
 			}
-			
+
 			if(Sum < sum) {
 				Sum = sum;
 				Answer = i;
