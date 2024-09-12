ee61a3e201805fd2f911fdb558bb0f7bdcbd21de
사한 <brian_als@naver.com>
2024-09-12 21:53:21 +0900

    [Gold V] Title: LCS, Time: 204 ms, Memory: 29548 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -14,7 +14,44 @@ public class Main {
 
         map = new Integer[s1.length()][s2.length()];
 
-        System.out.print(LCS(s1.length() - 1, s2.length() - 1));
+        System.out.println(LCS(s1.length() - 1, s2.length() - 1));
+
+
+/*      만약 길이가 아니라 직접 LCS를 구해야한다면?
+        https://lotuslee.tistory.com/39 해당 블로그 참조
+
+        StringBuilder sb = new StringBuilder();
+        int i = s1.length() - 1;
+        int j = s2.length() - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (i - 1 >= 0 && map[i][j] == map[i - 1][j]) {
+                i--;
+                continue;
+            }
+
+            if (j - 1 >= 0 && map[i][j] == map[i][j - 1]) {
+                j--;
+                continue;
+            }
+
+            if (i - 1 >= 0 && j - 1 >= 0 && map[i][j] == map[i - 1][j - 1] + 1) {
+                sb.append(s1.charAt(i));
+                i--;
+                j--;
+                continue;
+            }
+
+            if(i - 1 <= 0 || j - 1 <= 0) {
+                if(map[i][j] == 1 && s1.charAt(i) == s2.charAt(j)) {
+                    sb.append(s1.charAt(i));
+                    i--;
+                    j--;
+                }
+            }
+        }
+
+        System.out.println(sb.reverse());*/
     }
 
     static int LCS(int row, int col) {
