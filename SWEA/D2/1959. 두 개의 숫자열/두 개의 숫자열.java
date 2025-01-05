10d2587f400c23ce7791b387c698f06a86d1cbc5
사한 <brian_als@naver.com>
2025-01-05 19:36:22 +0900

    [D2] Title: 두 개의 숫자열, Time: 106 ms, Memory: 23,916 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
@@ -3,56 +3,80 @@ import java.util.*;
 
 public class Solution {
 
+    static int N;
+    static int M;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
         int T = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
 
-            int[] arrayN = new int[N];
-            int[] arrayM = new int[M];
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+
+            int[] Aj = new int[N];
+            int[] Bj = new int[M];
 
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                int parseInt = Integer.parseInt(st.nextToken());
-                arrayN[j] = parseInt;
+                int value = Integer.parseInt(st.nextToken());
+                Aj[j] = value;
             }
 
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                int parseInt = Integer.parseInt(st.nextToken());
-                arrayM[j] = parseInt;
+                int value = Integer.parseInt(st.nextToken());
+                Bj[j] = value;
             }
 
-            int max = 0;
-            if (N > M) {
-                for (int j = 0; j <= N - M; j++) {
-                    int sum = 0;
-                    for (int k = 0; k < M; k++) {
-                        sum += arrayN[k + j] * arrayM[k];
-                    }
-                    if (sum > max) {
-                        max = sum;
-                    }
-                }
-            } else {
-                for (int j = 0; j <= M - N; j++) {
-                    int sum = 0;
-                    for (int k = 0; k < N; k++) {
-                        sum += arrayM[k + j] * arrayN[k];
-                    }
-                    if (sum > max) {
-                        max = sum;
-                    }
-                }
-            }
-            sb.append("#").append(i + 1).append(" ").append(max).append("\n");
+            int result = calculate(Aj, Bj);
+            sb.append("#").append(i+1).append(" ").append(result).append("\n");
         }
-        System.out.print(sb);
+
+        System.out.println(sb);
+    }
+
+    public static int calculate(int[] aj, int[] bj) {
+
+        int max = 0;
+        int weight = 0;
+        if (N > M) {
+
+            while (true) {
+                int sum = 0;
+
+                if (weight + M > N) {
+                    break;
+                }
+
+                for (int i = 0; i < M; i++) {
+                    sum += bj[i] * aj[i+weight];
+                }
+
+                max = Integer.max(max, sum);
+                weight++;
+            }
+        }else {
+
+            while (true) {
+                int sum = 0;
+
+                if (weight + N > M) {
+                    break;
+                }
+
+                for (int i = 0; i < N; i++) {
+                    sum += aj[i] * bj[i+weight];
+                }
+                max = Integer.max(max, sum);
+                weight++;
+            }
+        }
+
+        return max;
     }
 
 
