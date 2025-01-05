75be853381160c45f2ed73a2ac1aa295647cc34b
사한 <brian_als@naver.com>
2025-01-05 19:36:41 +0900

    [D2] Title: 숫자 배열 회전, Time: 100 ms, Memory: 27,384 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
@@ -3,57 +3,69 @@ import java.util.*;
 
 public class Solution {
 
+    static int N;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
         int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
-            int N = Integer.parseInt(br.readLine());
 
-            int[][] map = new int[N][N];
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            N = Integer.parseInt(st.nextToken());
+
+            ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
             for (int j = 0; j < N; j++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
+                arr.add(new ArrayList<>());
+            }
+
+            for (int j = 0; j < N; j++) {
+                st = new StringTokenizer(br.readLine());
                 for (int k = 0; k < N; k++) {
                     int parseInt = Integer.parseInt(st.nextToken());
-                    map[j][k] = parseInt;
+                    arr.get(j).add(parseInt);
                 }
             }
 
-            int[][] rotation90 = rotation(map);
-            int[][] rotation180 = rotation(rotation90);
-            int[][] rotation270 = rotation(rotation180);
+            ArrayList<ArrayList<Integer>> turn90 = turn(arr);
+            ArrayList<ArrayList<Integer>> turn180 = turn(turn90);
+            ArrayList<ArrayList<Integer>> turn270 = turn(turn180);
 
-            sb.append("#").append(i + 1).append("\n");
+            sb.append("#").append(i+1).append("\n");
 
             for (int j = 0; j < N; j++) {
                 for (int k = 0; k < N; k++) {
-                    sb.append(rotation90[j][k]);
+                    Integer value = turn90.get(j).get(k);
+                    sb.append(value);
                 }
                 sb.append(" ");
-
                 for (int k = 0; k < N; k++) {
-                    sb.append(rotation180[j][k]);
+                    Integer value = turn180.get(j).get(k);
+                    sb.append(value);
                 }
                 sb.append(" ");
-
                 for (int k = 0; k < N; k++) {
-                    sb.append(rotation270[j][k]);
+                    Integer value = turn270.get(j).get(k);
+                    sb.append(value);
                 }
                 sb.append("\n");
             }
-
         }
-        System.out.print(sb);
 
+        System.out.println(sb);
     }
 
-    static int[][] rotation(int[][] map) {
-        int[][] result = new int[map.length][map.length];
+    public static ArrayList<ArrayList<Integer>> turn(ArrayList<ArrayList<Integer>> arr) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            result.add(new ArrayList<>());
+        }
 
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map.length; j++) {
-                result[i][j] = map[map.length - j - 1][i];
+        for (int i = 0; i < N; i++) {
+            for (int j = N -1; j >= 0; j--) {
+                Integer value = arr.get(j).get(i);
+                result.get(i).add(value);
             }
         }
 
