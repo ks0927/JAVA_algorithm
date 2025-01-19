ea1452f27ec20a9b6635df83f0a00e3fe5ae0511
사한 <brian_als@naver.com>
2025-01-19 16:26:19 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 196 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int N = sc.nextInt();
+
+        for (int i = 0; i < 2 * N - 1; i++) {
+
+            for (int j = 0; j < -Math.abs(i - (N - 1)) + N - 1; j++) {
+                sb.append(" ");
+            }
+
+            for (int k = 0; k < 2 * N - 1 - 2 * (-Math.abs(i - (N - 1)) + N - 1); k++) {
+                sb.append("*");
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+
+}
