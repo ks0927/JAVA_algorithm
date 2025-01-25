7df06231fec4bf74363ba62fde7a7f76c59d142d
사한 <brian_als@naver.com>
2025-01-26 01:15:19 +0900

    [Silver III] Title: N과 M (7), Time: 572 ms, Memory: 123720 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int[] p;
+	static int[] select;
+	static boolean[] visited;
+	static int M;
+	static int N;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		p = new int[N];
+		select = new int[M];
+		visited = new boolean[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			p[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(p);
+
+		perm(0);
+		System.out.println(sb);
+
+	}
+
+	public static void perm(int depth) {
+		if (depth == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(select[i]).append(" ");
+			}
+			sb.append("\n");
+
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+//			if(visited[i]) continue;
+			
+			visited[i] = true;
+			select[depth] = p[i];
+			perm(depth + 1);
+			visited[i] = false;
+			select[depth] = 0;
+		}
+	}
+}
