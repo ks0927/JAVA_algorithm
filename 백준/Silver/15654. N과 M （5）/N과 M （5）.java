34b9b2e94917d885794aedcd20037040c5c3ccdd
사한 <brian_als@naver.com>
2025-01-26 01:14:44 +0900

    [Silver III] Title: N과 M (5), Time: 292 ms, Memory: 28436 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -3,51 +3,55 @@ import java.util.*;
 
 public class Main {
 
-    static int[] lines;
-    static int[] map;
-    static boolean[] visited;
-    static StringBuilder sb = new StringBuilder();
+	static int[] p;
+	static int[] select;
+	static boolean[] visited;
+	static int M;
+	static int N;
+	static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws Exception {
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        map = new int[N];
-        visited = new boolean[N];
-        lines = new int[M];
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            map[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(map);
+		p = new int[N];
+		select = new int[M];
+		visited = new boolean[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			p[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(p);
 
-        DFS(N, M, 0);
+		perm(0);
+		System.out.println(sb);
 
-        System.out.println(sb);
-    }
+	}
 
-    static void DFS(int N, int M, int depth) {
-        if (depth == M) {
-            for (int i = 0; i < M; i++) {
-                sb.append(lines[i]).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
+	public static void perm(int depth) {
+		if (depth == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(select[i]).append(" ");
+			}
+			sb.append("\n");
 
-        for (int i = 0; i < N; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                lines[depth] = map[i];
-                DFS(N, M, depth + 1);
-                visited[i] = false;
-            }
-        }
-    }
+			return;
+		}
 
-
+		for (int i = 0; i < N; i++) {
+			if(visited[i]) continue;
+			
+			visited[i] = true;
+			select[depth] = p[i];
+			perm(depth + 1);
+			visited[i] = false;
+			select[depth] = 0;
+		}
+	}
 }
