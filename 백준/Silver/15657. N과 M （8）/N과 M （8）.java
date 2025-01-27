c3b29c0f8be59826306dc49698682069bd9e37f6
사한 <brian_als@naver.com>
2025-01-27 19:49:15 +0900

    [Silver III] Title: N과 M (8), Time: 148 ms, Memory: 17332 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -2,46 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    static int[] lines;
+    static int N;
+    static int M;
     static int[] map;
-    static StringBuilder sb = new StringBuilder();
+    static int[] pick;
+    static StringBuilder sb;
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         map = new int[N];
-        lines = new int[M];
+        pick = new int[M];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            map[i] = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+            map[i] = value;
         }
+
+        sb = new StringBuilder();
         Arrays.sort(map);
 
-        DFS(N, M, 0, 0);
+        combi(0, 0);
 
-        System.out.println(sb);
+        System.out.print(sb);
     }
 
-    static void DFS(int N, int M, int depth, int at) {
+    public static void combi(int depth, int start) {
         if (depth == M) {
-            for (int i = 0; i < M; i++) {
-                sb.append(lines[i]).append(" ");
+            for (int value : pick) {
+                sb.append(value).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for (int i = at; i < N; i++) {
-            lines[depth] = map[i];
-            DFS(N, M, depth + 1, i);
+        for (int i = start; i < N; i++) {
+            pick[depth] = map[i];
+            combi(depth + 1, i);
+            pick[depth] = 0;
         }
+
     }
-
-
 }
