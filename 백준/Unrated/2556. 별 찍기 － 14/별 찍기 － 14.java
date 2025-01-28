486eee2630520d85bade15327b9646ff8393a1e9
사한 <brian_als@naver.com>
2025-01-28 18:48:24 +0900

    [Unrated] Title: 별 찍기 - 14, Time: 112 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Unrated/2556. 별 찍기 － 14/별 찍기 － 14.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+
+            for (int j = 0; j < N; j++) {
+                sb.append("*");
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+
+}
