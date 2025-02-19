d56e6b49f9972b9bbc912429a08b6702eff5e814
사한 <brian_als@naver.com>
2025-02-19 22:27:51 +0900

    [D2] Title: 달팽이 숫자, Time: 80 ms, Memory: 24,320 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -1,99 +1,65 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
-class Solution
-{
-    
-    
-    static int[] directionX = {1, 0, -1, 0};
-    static int[] directionY = {0, 1, 0, -1};
-    
-	public static void main(String args[]) throws Exception
-	{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+public class Solution {
+	static int[] dr = { 0, 1, 0, -1 };
+	static int[] dc = { 1, 0, -1, 0 };
+	static int[][] map;
+	static int N;
 
-        int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
-            int N = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
 
-            int[][] map = new int[N][N];
+		for (int i = 0; i < T; i++) {
+			N = Integer.parseInt(br.readLine());
+			map = new int[N][N];
 
-            int count = 0;
-            int x = 0;
-            int y = 0;
-            int value = 1;
-            int flag = 0;
-            while (count != N * N) {
-                map[y][x] = value++;
-                count++;
-                int nextX = x + directionX[flag];
-                int nextY = y + directionY[flag];
+			makeSnail(N, i + 1);
+			
+			sb.append("#").append(i+1).append("\n");
+			
+			for (int j = 0; j < N; j++) {
+				for (int j2 = 0; j2 < N; j2++) {
+					sb.append(map[j][j2]).append(" ");
+				}
+				sb.append("\n");
+			}
+		}
 
-                if (0 > nextX || nextX >= N || 0 > nextY || nextY >= N || map[nextY][nextX] != 0) {
-                    flag = changeFlag(flag);
-                    x += directionX[flag];
-                    y += directionY[flag];
-                    continue;
-                }
-
-                x = nextX;
-                y = nextY;
-            }
-
-            sb.append("#").append(i + 1).append("\n");
-            for (int j = 0; j < N; j++) {
-                for (int k = 0; k < N; k++) {
-                    sb.append(map[j][k]).append(" ");
-                }
-                sb.append("\n");
-            }
-        }
-
-        System.out.print(sb);
+		System.out.println(sb);
 	}
-    
-        static int changeFlag(int flag) {
-        if (flag == 0)
-            return 1;
-        if (flag == 1)
-            return 2;
-        if (flag == 2)
-            return 3;
-        if (flag == 3)
-            return 0;
 
-        return -1;
-    }
+	private static void makeSnail(int N, int index) {
+		int r = 0;
+		int c = -1;
+
+		int flag = 0;
+		int cnt = 0;
+		
+		while(cnt < N*N) {
+			int nr = r + dr[flag];
+			int nc = c + dc[flag];
+			
+			if(turnCheck(nr,nc)) {
+				flag = (flag + 1) % 4;
+				continue;
+			}
+			cnt++;
+			map[nr][nc] = cnt;
+			r = nr;
+			c = nc;
+		}
+	}
+
+	private static boolean turnCheck(int r, int c) {
+		if (!(r >= 0 && r < N && c >= 0 && c < N) || map[r][c] != 0) {
+			return true;
+		}
+		return false;
+	}
+
 }
