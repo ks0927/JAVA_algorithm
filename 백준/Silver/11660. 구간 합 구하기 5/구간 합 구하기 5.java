3b6ba833ebdd23737fb6d5c1b29055a07a64667d
사한 <brian_als@naver.com>
2025-02-20 22:49:22 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 676 ms, Memory: 135324 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -2,87 +2,79 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	static int[][] map;
+	static int[][] sumList;
+	static int N;
+	static int M;
 
-    static int[][] map;
-    static int[][] dp;
+	public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+		map = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
-        map = new int[N][N];
-        dp = new int[N][N]; // 세로 즉 열의 합을 담고있다.
+		sumList = new int[N][N];
+		initMap();
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                int value = Integer.parseInt(st.nextToken());
-                map[i][j] = value;
-            }
-        }
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int r1 = Integer.parseInt(st.nextToken()) - 1;
+			int c1 = Integer.parseInt(st.nextToken()) - 1;
+			int r2 = Integer.parseInt(st.nextToken()) - 1;
+			int c2 = Integer.parseInt(st.nextToken()) - 1;
 
-        // dp배열 초기화
-        for (int i = 0; i < N; i++) {
-            int sum = 0;
-            for (int j = 0; j < N; j++) {
-                sum += map[j][i];
-                dp[j][i] = sum;
-            }
-        }
+			int result = partitionSum(r1, c1, r2, c2);
 
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
+			sb.append(result).append("\n");
+		}
 
-            int result = calculate(x1 - 1, y1 - 1, x2 - 1, y2 - 1);
-            sb.append(result).append("\n");
-        }
+		System.out.println(sb);
+	}
 
-        System.out.print(sb);
+	private static void initMap() {
+		sumList = new int[N][N];
 
-    }
+		// 가로 세로 초기화
+		int sum = 0;
+		int sum2 = 0;
+		for (int i = 0; i < N; i++) {
+			sum += map[0][i];
+			sumList[0][i] = sum;
 
-    // 세로의 합인 dp를 순회한다.
-    // 단 행이 x2일때 y1열부터 y2열까지 더한뒤
-    // x1행이 0이 아니면 x1 - 1 행의 y1열 부터 y2열 까지 빼준다.
-    //    예를 들자면 다음과 같은 표가 있다면
-    //            1 2 3 4
-    //            2 3 4 5
-    //            3 4 5 6
-    //            4 5 6 7
-    //    dp는 다음과 같이 구성된다.
-    //        1 2 3 4
-    //        3 5 7 9
-    //        6 9 12 15
-    //        10 14 18 22
-    //    이때 (2,2) 부터 (4,4) 까지 구한다고 생각하면
-    //            1  2 3  4
-    //            2 |3 4| 5
-    //            3 |4 5| 6
-    //            4 |5 6| 7
-    //    표에서 해당 값을 구해야한다. 따라서 dp에서는
-    //    14 와 18을 더한뒤 2와 3을 빼면 된다.
-    static int calculate(int x1, int y1, int x2, int y2) {
-        int result = 0;
+			sum2 += map[i][0];
+			sumList[i][0] = sum2;
+		}
 
-        for (int i = y1; i <= y2; i++) {
-            result += dp[x2][i];
-        }
+		// 나머지 부분 초기화
+		for (int i = 1; i < N; i++) {
+			for (int j = 1; j < N; j++) {
+				sumList[i][j] = sumList[i - 1][j] + sumList[i][j - 1] - sumList[i - 1][j - 1] + map[i][j];
+			}
+		}
 
-        if (x1 != 0) {
-            for (int i = y1; i <= y2; i++) {
-                result -= dp[x1 - 1][i];
-            }
-        }
-        return result;
+	}
 
-    }
+	private static int partitionSum(int r1, int c1, int r2, int c2) {
+		int sum = sumList[r2][c2];
+
+		if (r1 > 0)
+			sum -= sumList[r1 - 1][c2];
+		if (c1 > 0)
+			sum -= sumList[r2][c1 - 1];
+		if (r1 > 0 && c1 > 0)
+			sum += sumList[r1 - 1][c1 - 1];
+
+		return sum;
+	}
 
 }
