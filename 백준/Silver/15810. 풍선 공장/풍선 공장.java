5772395725b945a752f0dd5c345bd722c0517ec7
사한 <brian_als@naver.com>
2025-04-03 16:58:21 +0900

    [Silver II] Title: 풍선 공장, Time: 952 ms, Memory: 301880 KB -BaekjoonHub

---
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N, M;
+	static int[] ball;
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		ball = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			ball[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Long left = 0L;
+		Long right = 1000000000000L;
+		Long mid;
+		while (left < right) {
+			mid = (left + right) / 2;
+
+			Long sum = 0L;
+			for (int i = 0; i < N; i++) {
+				sum += mid / ball[i];
+			}
+			
+			if(sum < M) {
+				left = mid + 1;
+			}else {
+				right = mid;
+			}
+		}
+		
+		System.out.println(right);
+	}
+
+}
