a8204850c64677f135a7abaa79c9e0d30b853d43
사한 <brian_als@naver.com>
2025-04-07 11:56:28 +0900

    [Gold V] Title: 평범한 배낭, Time: 124 ms, Memory: 13228 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -7,7 +7,7 @@ public class Main {
 	static int[] weights;
 	static int[] profits;
 
-	static int[][] dp;
+	static int[] dp;
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,19 +28,19 @@ public class Main {
 			profits[i] = p;
 		}
 
-		dp = new int[N+1][K+1];
+		dp = new int[K+1];
 		
 		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= K; j++) {
-				
-				if(weights[i] <= j) { // j인 가방 보다 내 무게가 작거나 같을때는 넣어봐야지
-					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-weights[i]] + profits[i]);
-				}else { // j인가방 보다 내 무게가 크면 아예 못넣으니까 이전값 가져와야지
-					dp[i][j] = dp[i-1][j];
-				}
+			int w = weights[i];
+			int p = profits[i];
+
+			for (int j = K; j >= w; j--) {
+				dp[j] = Math.max(dp[j], dp[j - w] + p);
 			}
+			
 		}
-		System.out.println(dp[N][K]);
+		
+		System.out.println(dp[K]);
 		
 	}
 
