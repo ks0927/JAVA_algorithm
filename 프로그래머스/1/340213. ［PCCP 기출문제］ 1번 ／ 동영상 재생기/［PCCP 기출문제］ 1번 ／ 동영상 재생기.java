00ac319199b16a1a335b210dc558fe779d89f402
사한 <brian_als@naver.com>
2025-04-14 22:00:35 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 1.12 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,51 +1,53 @@
-import java.util.*;
-import java.io.*;
-
 class Solution {
-    static int  totalOpS;
-    static int  totalOpE;
-    
-    public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) { 
+    public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
         
-        int result = 0;
-        int totalVid = convertTotalSec(video_len);
-        int totalPos = convertTotalSec(pos);
-        totalOpS = convertTotalSec(op_start);
-        totalOpE = convertTotalSec(op_end);
-        
-        
-        result = checkSkip(totalPos);
-        
-        for(int i = 0; i < commands.length; i++) {
-            if(commands[i].equals("next")) {
-                result = (totalVid < result + 10 ) ? totalVid : result + 10;
-            }
-            if(commands[i].equals("prev")) {
-                result = (0 > result - 10 ) ? 0 : result - 10;
-            }
-            
-            result = checkSkip(result);
+        int VL = convertSecond(video_len);
+        int PS = convertSecond(pos);
+        int OS = convertSecond(op_start);
+        int OE = convertSecond(op_end);
+
+        if(PS >= OS && PS <= OE) {
+            PS = OE;
         }
         
-        String answer = String.format("%02d:%02d",result / 60, result % 60);
+        for(String cmm : commands) {
+            if(cmm.equals("prev")) {
+                
+                if(PS-10 < 0) {
+                    PS = 0;
+                }else{
+                    PS -= 10;
+                } 
 
+            }
+            
+            if(cmm.equals("next")) {
+                
+                if(PS+10 > VL) {
+                    PS = VL;
+                }else{
+                    PS += 10;
+                } 
+            }
+            if(PS >= OS && PS <= OE) {
+                PS = OE;
+            }
+        }
+        
+        
+        String answer = String.format("%02d:%02d",PS/60,PS%60);
+        
         return answer;
     }
     
-    static public int convertTotalSec(String time) {
-        String[] split_time = time.split(":");
+    public static int convertSecond(String str) {
+        String[] parse = str.split(":");
         
-        int min = Integer.parseInt(split_time[0]);
-        int sec = Integer.parseInt(split_time[1]);
+        int sum = 0;
         
-        return min*60 + sec;
-    }
-    
-    static public int checkSkip(int time) {
-        if(time <= totalOpE && time >= totalOpS) {
-            return totalOpE;
-        }
-        
-        return time;
+        sum += (Integer.parseInt(parse[0]) * 60);
+        sum += (Integer.parseInt(parse[1]));
+                
+        return sum;
     }
 }
