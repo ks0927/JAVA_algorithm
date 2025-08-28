da06f1ba47b405abd6256d87b423c5d7899fab19
사한 <brian_als@naver.com>
2025-08-28 13:54:02 +0900

    [Bronze I] Title: 2진수 8진수, Time: 296 ms, Memory: 79212 KB -BaekjoonHub

--- 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -1,16 +1,19 @@
 import java.util.*;
 import java.io.*;
-import java.math.BigInteger;
 
 public class Main {
-
-	public static void main(String[] args) throws Exception {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		String value = br.readLine();
-
-		System.out.println(new BigInteger(value,2).toString(8));
-	}
-
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String value = br.readLine();
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = value.length(); i > 0; i -= 3) {
+            int start = Math.max(0, i - 3);
+            String substring = value.substring(start, i);
+            sb.append(Integer.toString(Integer.parseInt(substring, 2), 8));
+        }
+        
+        System.out.println(sb.reverse());
+    }
 }
