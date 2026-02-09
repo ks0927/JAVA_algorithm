38c9db7eab3f91999beed7b1d58a5f5fc63e097a
사한 <brian_als@naver.com>
2026-02-09 11:44:16 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 444 ms, Memory: 53496 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,44 +1,36 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-
-    public static void main(String[] args) throws IOException {
+class Main {
+    public static void main(String[] args) throws Exception {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
+        
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
-        Map<String,Integer> map = new HashMap<>();
-        String[] strings = new String[N+1];
-        for (int i = 1; i <= N; i++) {
-            String readLine = br.readLine();
-            strings[i] = readLine;
-            map.put(readLine,i);
+        
+        
+        HashMap<String,Integer> map = new HashMap<>();
+        String[] map2 = new String[N+1];
+        for(int i=1; i <= N; i++) {
+            String pocket = br.readLine();
+            map.put(pocket,i);
+            map2[i] = pocket;
         }
-
-        for (int i = 0; i < M; i++) {
-            String readLine = br.readLine();
-            if(isDigit(readLine)){
-                sb.append(strings[Integer.parseInt(readLine)]).append("\n");
-                continue;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i < M; i++) {
+            String op = br.readLine();
+            if(Character.isDigit(op.charAt(0))) {
+                int a = Integer.parseInt(op);
+                sb.append(map2[a]).append("\n");
+                
+            }else{
+                sb.append(map.get(op)).append("\n");
             }
-            sb.append(map.get(readLine)).append("\n");
         }
-
-        System.out.println(sb);
+        System.out.print(sb);
     }
-
-    private static boolean isDigit(String readLine) {
-        try {
-            Integer.parseInt(readLine);
-            return true;
-        }catch (NumberFormatException e) {
-            return false;
-        }
-    }
-
-
 }
