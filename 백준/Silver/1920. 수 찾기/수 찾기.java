0b8ed0f03802866a535cf3a0eb60993cb673b2d0
사한 <brian_als@naver.com>
2026-02-09 14:11:41 +0900

    [Silver IV] Title: 수 찾기, Time: 464 ms, Memory: 60452 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,53 +1,29 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+class Main {
+    public static void main(String[] args) throws Exception {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         int N = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[N];
+        HashMap<Integer,Boolean> map = new HashMap<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for(int i=0; i < N; i++) {
+            int cur = Integer.parseInt(st.nextToken());
+            map.put(cur,true);
         }
-        Arrays.sort(arr);
-
+        
+        StringBuilder sb = new StringBuilder();
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            int parseInt = Integer.parseInt(st.nextToken());
-            if(binarySearch(arr, parseInt) >= 0){
+        for(int i=0; i < M; i++) {
+            int cur = Integer.parseInt(st.nextToken());
+            if(map.containsKey(cur)){
                 sb.append(1).append("\n");
-            }else {
+            }else{
                 sb.append(0).append("\n");
             }
         }
         System.out.println(sb);
-    }
-
-    public static int binarySearch(int[] arr, int parseInt) {
-
-        int left = 0;
-        int right = arr.length - 1;
-
-        while (left <= right) {
-
-            int middle = (left + right) / 2;
-
-            if (parseInt < arr[middle]) {
-                right = middle - 1;
-            } else if (parseInt > arr[middle]) {
-                left = middle + 1;
-            } else {
-                //찾으면 인덱스값 반환.
-                return middle;
-            }
-        }
-        //못찾음
-        return -1;
     }
 }
