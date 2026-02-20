2784a9a564133e32bfd0581d32b3e6bd74e76deb
사한 <brian_als@naver.com>
2026-02-20 13:36:33 +0900

    [Silver III] Title: 두 수의 합, Time: 236 ms, Memory: 26252 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,49 +1,46 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-
-    static StringBuilder sb = new StringBuilder();
+class Main {
 
     public static void main(String[] args) throws Exception {
-
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        
         int N = Integer.parseInt(br.readLine());
-
-        int[] map = new int[N];
+        
+        int[] arr = new int[N];
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            map[i] = Integer.parseInt(st.nextToken());
+        for(int i=0 ; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(arr);
+        
         int X = Integer.parseInt(br.readLine());
-
-        Arrays.sort(map);
-
-        int leftIndex = 0;
-        int rightIndex = N - 1;
-
+        
+        int s = 0;
+        int e = N-1;
+        
         int cnt = 0;
-        while (rightIndex > leftIndex) {
-            int sum = map[leftIndex] + map[rightIndex];
-
-            if (sum > X) {
-                rightIndex--;
+        while ( s < e ){
+            int cur = arr[s] + arr[e];
+            
+            if(cur > X) {
+                e--;
             }
-            if (sum == X) {
+            if(cur < X){
+                s++;
+            }
+            if(cur == X){
                 cnt++;
-                leftIndex++;
-                rightIndex--;
-            }
-            if (sum < X) {
-                leftIndex++;
+                e--;
+                s++;
             }
         }
-
+        
         System.out.println(cnt);
-
     }
 
 }
-
-
