caf878781d8c56bec55b4bb4a51ebcc1e0e8d258
사한 <brian_als@naver.com>
2026-02-20 13:02:52 +0900

    [Silver III] Title: N과 M (1), Time: 144 ms, Memory: 21004 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,54 +1,48 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-
-	static int[] p;
-	static int[] select;
-	static boolean[] visited;
-	static int M;
-	static int N;
-	static StringBuilder sb = new StringBuilder();
-
-	public static void main(String[] args) throws Exception {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-
-		p = new int[N];
-		select = new int[M];
-		visited = new boolean[N];
-		
-		for (int i = 0; i < N; i++) {
-			p[i] = i+1;
-		}
-
-		perm(0);
-		System.out.println(sb);
-
-	}
-
-	public static void perm(int depth) {
-		if (depth == M) {
-			for (int i = 0; i < M; i++) {
-				sb.append(select[i]).append(" ");
-			}
-			sb.append("\n");
-
-			return;
-		}
-
-		for (int i = 0; i < N; i++) {
-			if(visited[i]) continue;
-			
-			visited[i] = true;
-			select[depth] = p[i];
-			perm(depth + 1);
-			visited[i] = false;
-			select[depth] = 0;
-		}
-	}
+class Main {
+    static int N,M;
+    static int[] p,visited;
+    static StringBuilder sb;
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        p = new int[M];
+        visited = new int[N+1];
+        sb = new StringBuilder();
+        
+        perm(0);
+        
+        System.out.print(sb);
+    }
+    
+    public static void perm(int depth) {
+        if(depth == M) {
+            
+            for(int i= 0; i < M-1; i++) {
+                sb.append(p[i]).append(" ");
+            }
+            sb.append(p[M-1]);
+            sb.append("\n");
+            
+            return;
+        }
+        
+        for(int i=1; i <= N; i++) {
+            if(visited[i] ==1) continue;
+                
+            visited[i] = 1;
+            p[depth] = i;
+            perm(depth+1);
+            visited[i] = 0;
+            
+        }
+    }
 }
