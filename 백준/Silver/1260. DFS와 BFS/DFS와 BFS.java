014c32f8bca7acbfdbbfc29c7b6e7a9b7751bfd2
사한 <brian_als@naver.com>
2026-02-23 12:21:42 +0900

    [Silver II] Title: DFS와 BFS, Time: 204 ms, Memory: 21488 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,87 +1,86 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-
-    static int N;
-    static int M;
-    static int V;
+class Main {
+    static int N,M,V;
     static int[][] map;
-    static ArrayList<Integer> dfsValue;
-    static ArrayList<Integer> bfsValue;
-    static boolean[] visited;
-
-
-    public static void main(String[] args) throws IOException {
-
+    static int[] visited;
+    static ArrayList<Integer> bfs,dfs;
+    public static void main(String[] args) throws Exception{
+        StringBuilder sb = new StringBuilder();
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         V = Integer.parseInt(st.nextToken());
-
+        
         map = new int[N+1][N+1];
-        visited = new boolean[N+1];
-
-        dfsValue = new ArrayList<>();
-        bfsValue = new ArrayList<>();
-
-        for (int i = 0; i < M; i++) {
+        dfs = new ArrayList<>();
+        visited = new int[N+1];
+        
+        for(int i=0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int v1 = Integer.parseInt(st.nextToken());
-            int v2 = Integer.parseInt(st.nextToken());
-            map[v1][v2] = 1;
-            map[v2][v1] = 1;
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            
+            map[s][e] = 1;
+            map[e][s] = 1;
         }
-        DFS(V);
-        for (Integer integer : dfsValue) {
-            System.out.print(integer+" ");
-        }
+        
+        dfs.add(V);
+        visited[V] = 1;
+        dfs(V);
 
-        System.out.println();
-        visited = new boolean[N+1];
-        BFS(V);
-        for (Integer integer : bfsValue) {
-            System.out.print(integer+" ");
+        for(int cur : dfs) {
+            sb.append(cur).append(" ");
         }
+        sb.deleteCharAt(sb.length()-1);
+        sb.append("\n");
 
+        bfs = new ArrayList<>();
+        visited = new int[N+1];
+        bfs(V);
+        for(int cur : bfs) {
+            sb.append(cur).append(" ");
+        }
+        sb.deleteCharAt(sb.length()-1);
+
+        System.out.println(sb);
     }
-    static void DFS(int n){
-        dfsValue.add(n);
-        visited[n] = true;
-        for (int i = 1; i <= N; i++) {
-
-            if(map[n][i]==1 && !visited[i]){
-                DFS(i);
+    
+    public static void dfs(int cur) {
+  
+        for(int i=1; i <= N; i++) {
+            if(map[cur][i] == 1 && visited[i] == 0) {
+                visited[i] = 1;
+                dfs.add(i);
+                dfs(i);
             }
         }
     }
-    static void BFS(int n){
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(n);
-        visited[n] = true;
-        bfsValue.add(n);
-        int level = 0;
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            ++level;
-            Integer poll = queue.poll();
-            for (int i = 0; i < size; i++) {
-                for (int j = 1; j <= N; j++) {
-                    if(map[poll][j]==1 && !visited[j]){
-                        queue.add(j);
-                        visited[j] = true;
-                        bfsValue.add(j);
+    
+    public static void bfs(int start) {
+        Queue<Integer> que = new LinkedList<>();
+        que.add(start);
+        
+        while(!que.isEmpty()) {
+            int size = que.size();
+            
+            for(int i=0; i < size; i++) {
+                int cur = que.poll();
+                bfs.add(cur);
+                visited[cur] = 1;
+                
+                for(int j=1; j <= N; j++) {
+                    if(map[cur][j] == 1 &&visited[j] == 0) {
+                        que.add(j);
+                        visited[j] = 1;
                     }
                 }
             }
         }
-
     }
 }
