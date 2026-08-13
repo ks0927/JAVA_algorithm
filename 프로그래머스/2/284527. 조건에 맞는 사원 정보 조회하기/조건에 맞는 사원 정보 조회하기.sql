0716de8a660910850c4a5a548fcee927880f2eb9
사한 <brian_als@naver.com>
2026-08-13 12:30:18 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -2,6 +2,7 @@ SELECT se.SCORE, se.EMP_NO , he.EMP_NAME, he.POSITION , he.EMAIL
 FROM HR_EMPLOYEES he LEFT JOIN (
                                 SELECT EMP_NO , SUM(SCORE) AS SCORE
                                 FROM HR_GRADE
+                                WHERE 2022
                                 GROUP BY EMP_NO
                                     ) se ON he.EMP_NO = se.EMP_NO
 ORDER BY SCORE desc
