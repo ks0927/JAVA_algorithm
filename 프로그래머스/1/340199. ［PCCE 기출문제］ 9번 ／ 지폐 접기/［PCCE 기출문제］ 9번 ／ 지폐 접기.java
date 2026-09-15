2fcb7bb4b3ffa6f119ee3c6cdefd32cb3d184b19
사한 <brian_als@naver.com>
2026-09-15 16:57:35 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.04 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -1,30 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] wallet, int[] bill) {
         int answer = 0;
         
-        while(lengthCheck(wallet,bill)) {
-            if(bill[0] > bill[1]) {
+        while (!((bill[0] <= wallet[0] && bill[1] <= wallet[1])
+              || (bill[0] <= wallet[1] && bill[1] <= wallet[0]))) {
+
+            if (bill[0] > bill[1]) {
                 bill[0] /= 2;
-            }else{
+            } else {
                 bill[1] /= 2;
             }
-            
+
             answer++;
         }
         
         return answer;
-    }
-    
-    public static boolean lengthCheck(int[] wallet, int[] bill) {
-        int wallet_S = (wallet[0] < wallet[1]) ? wallet[0] : wallet[1];
-        int wallet_D = (wallet[0] > wallet[1]) ? wallet[0] : wallet[1];
-        
-        int bill_S = (bill[0] < bill[1]) ? bill[0] : bill[1];
-        int bill_D = (bill[0] > bill[1]) ? bill[0] : bill[1];
-        
-        if(bill_S > wallet_S || bill_D > wallet_D) {
-            return true;
-        }
-        return false;
     }
 }
